bef4530082b283ead874bbffefc90340e2226fb5
tarunji-123 <tarunbhadoriya141@gmail.com>
2023-06-16 15:30:07 +0530

    add delete Button & remove item from local storage

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-console.log("hii");
+
 
 var form = document.getElementById('my-form');
 var items = document.getElementById('items');
 
 form.addEventListener('submit',addItems);
+items.addEventListener('click',removeItem);
 
 function addItems(e){
     e.preventDefault();
@@ -14,9 +15,17 @@ function addItems(e){
 
     var li = document.createElement('li');
     li.className = "item";
+
+    var delBtn = document.createElement('input');
+    delBtn.type ="button";
+    delBtn.value = "delete";
+    delBtn.className = "delete";
+
     li.appendChild(document.createTextNode(name));
     li.appendChild(document.createTextNode("-"+email));
     li.appendChild(document.createTextNode("-"+phone));
+    li.appendChild(delBtn);
+
     console.log(li);
 
     items.appendChild(li);
@@ -32,5 +41,16 @@ function addItems(e){
     let myObj_serialized = JSON.stringify(myObj);
     localStorage.setItem(email,myObj_serialized);
 
-    
+}
+
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are you confirm?')){
+            var li = e.target.parentElement;
+            
+            var email = li.childNodes[1].textContent.substring(1);
+            items.removeChild(li);
+            localStorage.removeItem(email);
+        }
+    }
 }
